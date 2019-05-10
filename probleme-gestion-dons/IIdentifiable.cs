2ada9733a021f23738c8cf9987f37c47b7c0dd6a
smahot <steve.mahot@edu.devinci.fr>
2019-05-10 10:16:08 +0200

    creation classes don, objets, personnes

---
+++ probleme-gestion-dons/IIdentifiable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probleme_gestion_dons
+{
+    interface IIdentifiable
+    {
+        int Identifiant { get; set; }
+    }
+}
---
+++ probleme-gestion-dons/Personne_physique.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probleme_gestion_dons
+{
+    public abstract class Personne_physique:Personne
+    {
+        string prenom;
+        string adresse;
+
+        protected Personne_physique(string prenom, string adresse, string nom, int identifiant, string telephone) : base(nom, identifiant, telephone)
+        {
+            this.prenom = prenom;
+            this.adresse = adresse;
+        }
+    }
+}
